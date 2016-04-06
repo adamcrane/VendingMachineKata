559a95c6d27f5f9dc7ca8d1799e1be4a20d63ab2
Adam Crane <craneadam16@gmail.com>
2016-04-06 19:48:10 -0400

    Refactoring tests to add all products in setup

--- src/test/WhenSelectingAProduct.java
+++ src/test/WhenSelectingAProduct.java
@@ -1,22 +1,34 @@
 package test;
 
 import main.VendingMachine;
+import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class WhenSelectingAProduct {
 
+    private VendingMachine machine;
+
+    @Before
+    public void setUp() throws Exception {
+        machine = new VendingMachine();
+        machine.AddProduct("Chips", .5);
+        machine.AddProduct("Cola", 1.0);
+        machine.AddProduct("Candy", .65);
+    }
+
     @Test
     public void MachineHasColaPricedAtOneDollar(){
-        VendingMachine machine = new VendingMachine();
-        machine.AddProduct("Cola", 1.0);
         assertEquals(1.0, machine.GetPriceOf("Cola"),0.001);
     }
 
     @Test
     public void MachineHasChipsPricedAtFiftyCents(){
-        VendingMachine machine = new VendingMachine();
-        machine.AddProduct("Chips", .5);
         assertEquals(.5, machine.GetPriceOf("Chips"),0.001);
+    }
+
+    @Test
+    public void MachineHasCandyPricedAtSixtyFiveCents(){
+        assertEquals(.65, machine.GetPriceOf("Candy"),0.001);
     }
 }
