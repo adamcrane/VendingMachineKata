aefd72aa247f2ac3e3497c107b8cbf2803a0319e
Adam Crane <craneadam16@gmail.com>
2016-04-07 21:42:55 -0400

    Switching type of parameter of get change to big decimal because Java

--- src/test/WhenMakingChange.java
+++ src/test/WhenMakingChange.java
@@ -3,8 +3,8 @@ package test;
 import main.Coin;
 import org.junit.Test;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
-import java.util.List;
 
 import static main.Coin.*;
 import static org.junit.Assert.*;
@@ -12,39 +12,49 @@ public class WhenMakingChange {
 
     @Test
     public void BalanceOfTwentyFiveCentsReturnsOneQuarter(){
-        assertEquals(Quarter, GetChangeFor(.25).get(0));
+        assertEquals(Quarter, GetChangeFor(BigDecimal.valueOf(.25)).get(0));
     }
 
     @Test
     public void BalanceOfTenCentsReturnsOneDime(){
-        assertEquals(Dime, GetChangeFor(.10).get(0));
+        assertEquals(Dime, GetChangeFor(BigDecimal.valueOf(.10)).get(0));
     }
 
     @Test
     public void BalanceOfFiveCentsReturnsOneNickel(){
-        assertEquals(Nickel, GetChangeFor(.05).get(0));
+        assertEquals(Nickel, GetChangeFor(BigDecimal.valueOf(.05)).get(0));
     }
 
     @Test
     public void BalanceOfOneCentReturnsOnePenny(){
-        assertEquals(Penny, GetChangeFor(.01).get(0));
+        assertEquals(Penny, GetChangeFor(BigDecimal.valueOf(.01)).get(0));
     }
 
     @Test
     public void BalanceOfFiftyCentsReturnsTwoQuarters(){
-        ArrayList<Coin> coins = GetChangeFor(.5);
+        ArrayList<Coin> coins = GetChangeFor(BigDecimal.valueOf(.5));
         assertEquals(Quarter, coins.get(0));
         assertEquals(Quarter, coins.get(1));
     }
-    private ArrayList<Coin> GetChangeFor(double v) {
+
+    @Test
+    public void BalanceOfThirtyFiveCentsReturnsQuarterAndDime(){
+        ArrayList<Coin> coins = GetChangeFor(BigDecimal.valueOf(.35));
+        assertEquals(Quarter, coins.get(0));
+        assertEquals(Dime, coins.get(1));
+    }
+    private ArrayList<Coin> GetChangeFor(BigDecimal v) {
         ArrayList<Coin> coinsToReturn = new ArrayList<>();
-        while(v >= .25) {
+        while(v.compareTo(BigDecimal.valueOf(.25)) > -1) {
             coinsToReturn.add(Quarter);
-            v -= .25;
+            v = v.subtract(BigDecimal.valueOf(.25));
         }
-        if(v == .10) coinsToReturn.add(Dime);
-        if(v == .05) coinsToReturn.add(Nickel);
-        if(v == .01) coinsToReturn.add(Penny);
+        while(v.compareTo(BigDecimal.valueOf(.10)) > -1) {
+            coinsToReturn.add(Dime);
+            v = v.subtract(BigDecimal.valueOf(.10));
+        }
+        if(v.compareTo(BigDecimal.valueOf(.05)) == 0) coinsToReturn.add(Nickel);
+        if(v.compareTo(BigDecimal.valueOf(.01)) == 0) coinsToReturn.add(Penny);
 
         return coinsToReturn;
     }
